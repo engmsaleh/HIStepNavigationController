d49f34083480e51848046c2332135fdca508eb43
kshuin <koichi.skt@gmail.com>
2012-09-02 16:26:50 +0900

    Initial Commit

---
+++ HIStepNavigationController/HISAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HISAppDelegate.h
+//  HIStepNavigationController
+//
+//  Created by k-sakata on 9/2/12.
+//  Copyright (c) 2012 http://www.huin-lab.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HIStepNavigationController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HIStepNavigationController
+//
+//  Created by k-sakata on 9/2/12.
+//  Copyright (c) 2012 http://www.huin-lab.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HISAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([HISAppDelegate class]));
+  }
+}
